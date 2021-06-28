a541a90f6f1a1de6847914bfcf77fdee1b5a2bda
Alin Matei <alin.matei2442@gmail.com>
2021-06-28 17:54:46 +0300

    added nothing

--- app/src/main/java/com/example/myonlineshopp/Login.java
+++ app/src/main/java/com/example/myonlineshopp/Login.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -23,7 +24,6 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Intent intent = new Intent(this, app.class);
-
         //startActivity(intent);
 
     }
